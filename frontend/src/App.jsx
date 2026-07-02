ffd6fce47a8065431c0f0b0aedf4dc2a053ee830
Pedro Maia <maia.pedrodn@gmail.com>
2026-07-02 00:46:29 -0300

    feat: display Pokémon generation list

---
+++ frontend/src/App.jsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react";
+import { getGeneration } from "./services/api";
+import PokemonCard from "./components/PokemonCard";
+import GenerationTabs from "./components/GenerationTabs";
+import PokemonGrid from "./components/PokemonGrid";
+import SearchBar from "./components/SearchBar";
+
+
+function App() {
+  const [pokemons, setPokemons] = useState([]);
+  const [generation, setGeneration] = useState(1);
+  const [search, setSearch] = useState("");
+  const filteredPokemons = pokemons.filter((pokemon) =>
+    pokemon.name.toLowerCase().includes(search.toLowerCase())
+  );
+
+  useEffect(() => {
+    async function loadGeneration() {
+      try {
+        const data = await getGeneration(generation);
+        setPokemons(data.pokemons);
+      } catch (error) {
+        console.error("Erro ao buscar Pokémons:", error);
+      }
+    }
+
+    loadGeneration();
+  }, [generation]);
+  
+  return (
+    <div className="maindiv">
+      <header>
+        <h1>Pokédex</h1>
+        <SearchBar
+          search={search}
+          setSearch={setSearch}
+        />
+      
+      </header>
+
+      <GenerationTabs
+        generation={generation}
+        setGeneration={setGeneration}
+      />
+
+      <PokemonGrid
+        pokemons={filteredPokemons}
+      />
+
+
+    </div>
+  );
+}
+
+export default App;
